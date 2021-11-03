a3f543667154d91773b8582b526908e31182f395
mihgun107 <mihgun106@gmail.com>
2021-11-03 19:35:32 +0300

    Начало раздела 3 урока 4 - создание репозитория на Гитхаб

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,15 +18,15 @@
                 <div class="container-fluid">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link menu-link {{ $mainLink }} "
-                               href=" {{ route('home')  }}">Главная страница</a>
+                            <a class="nav-link menu-link menu-link__active "
+                               href="#">Главная страница</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link menu-link {{ $articleLink }}"
-                               href="{{ route('article.index')  }}">Каталог статей</a>
+                            <a class="nav-link menu-link"
+                               href="#">Каталог статей</a>
                         </li>
                     </ul>
-                    <a class="d-flex justify-content-end " href="https://github.com/rageserg">
+                    <a class="d-flex justify-content-end " href="https://github.com/sergrage">
                         <i class="bi bi-github" style="font-size: 2rem; color: #000000;"></i>
                     </a>
                 </div>
--- webpack.mix.js
+++ webpack.mix.js
@@ -15,5 +15,5 @@ mix.js('resources/js/app.js', 'public/js')
     .vue()
     .sass('resources/sass/app.scss', 'public/css').version();
 
-mix.copy('resouces/img','public/img');
+mix.copy('resources/img','public/img');
 
